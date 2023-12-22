12ad26d4a0b64b45b7cd608f34feb25947b2760d
Pushpa Yadav <pushpa.yadav@zapcg.com>
2023-12-22 16:24:11 +0530

    users.json

--- auth.js
+++ auth.js
@@ -3,22 +3,21 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Use middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Dummy user data (in a real system, you would use a database)
+
 const users = {
   'zap@example.com': 'secure123',
   'bob_user': 'strong123',
 };
 
-// Authentication function
+
 function authenticate(username, password) {
   return users[username] && users[username] === password;
 }
 
-// Middleware to check if the user is authenticated
+
 function isAuthenticated(req, res, next) {
   const { username, password } = req.body;
 
@@ -29,12 +28,12 @@ function isAuthenticated(req, res, next) {
   }
 }
 
-// Protected route
+
 app.get('/api/protected', isAuthenticated, (req, res) => {
   res.json({ message: 'You have access to this protected resource!' });
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
--- jwt.js
+++ jwt.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-// Create a JWT
 const payload = { user_id: 123, username: 'alice' };
 const secretKey = '1234567890';
 
@@ -8,7 +7,7 @@ const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
 
 console.log('Generated Token:', token);
 
-// Verify a JWT
+
 jwt.verify(token, secretKey, (err, decoded) => {
   if (err) {
     console.error('Verification Failed:', err.message);
